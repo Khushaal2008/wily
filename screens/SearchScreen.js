34165a57b5ca0e99f22d7dd04a2ab60d66f7166a
Khushaal2008 <75684230+Khushaal2008@users.noreply.github.com>
2021-08-11 07:25:26 +0530

    Add files via upload

---
+++ screens/SearchScreen.js
@@ -0,0 +1,146 @@
+import React from 'react';
+import { Text, View,TextInput,TouchableOpacity,StyleSheet,FlatList } from 'react-native';
+import db from '../Config'
+
+export default class Searchscreen extends React.Component {
+
+  constructor(){
+    super();
+    this.state={
+      search:'',
+      allTransactions:[],
+      lastVisibleTransaction:null
+    }
+  }
+
+  fetchMoreTransactions = async () =>{
+    var text = this.state.search.toUpperCase()
+    var enteredText =text.split ("")
+    if (enteredText[0].toUpperCase() === 'B'){
+        const query = await db.collection ("transactions") .where ('bookId', '==',text ).startAfter(this.state.lastVisibleTransaction).limit(10).get()
+        query . docs. map ((doc)=> {
+          this.setState({
+            allTransactions : [...this.state.allTransactions,doc.data()],
+            lastVisibleTransaction: doc
+
+          })
+        })
+    }
+
+    else if (enteredText[0].toUpperCase() === 'S'){
+const query = await db.collection("transactions").where("studentId", '==',text).startAfter(this.state.lastVisibleTransaction).limit(10).get()
+query.docs.map((doc)=>{
+this.setState({
+  allTransactions:[...this.state.allTransactions,doc.data()],
+  lastVisibleTransaction:doc
+})
+})
+    }
+  }
+
+
+
+
+    searchTransactions = async (text) =>{
+      var enteredText =text.split ("")
+      if (enteredText[0].toUpperCase() === 'B'){
+          const transaction = await db.collection ("transactions") .where ('bookId', '==',text ).limit(10).get()
+          transaction . docs. map ((doc)=> {
+            this.setState({
+              allTransactions : [...this.state.allTransactions,doc.data()],
+              lastVisibleTransaction: doc
+
+            })
+          })
+      }
+
+      else if (enteredText[0].toUpperCase() === 'S'){
+const transaction = await db.collection("transactions").where("studentId", '==',text).limit(10).get()
+transaction.docs.map((doc)=>{
+  this.setState({
+    allTransactions:[...this.state.allTransactions,doc.data()],
+    lastVisibleTransaction:doc
+  })
+})
+      }
+    }
+  
+  
+    render() {
+      return (
+        <View style={styles.container}>
+          <View style={styles.searchBar}>
+          <TextInput
+          style={styles.bar}
+          placeholder="Enter Book Id or Student Id"
+          onChangeText={(text)=>{
+            this.setState({
+              search:text,
+            })
+          }
+        }
+          />
+
+<TouchableOpacity style={styles.searchButton} onPress={()=>{this.searchTransactions(this.state.search)}}>
+            <Text>Search</Text>
+          </TouchableOpacity>
+
+          </View>
+          <FlatList
+            data = {this.state.allTransactions}
+            renderItem = {({item})=>(
+              <View style = {{borderBottomWidth : 2}}>
+                <Text>{"BookId :"+ item.bookId} </Text>
+                <Text>{"StudentId :"+item.studentId} </Text>
+                <Text>{"TransactionType" + item.transactionType} </Text>
+                <Text>{"Date" + item.data.toDate()} </Text>
+               </View>
+
+
+
+            )
+          }
+          keyExtractor ={(item,index)=> index.toString()}
+          onEndReached = {this.fetchMoreTransactions}
+          onEndReachedThreshold = {0.7}
+          
+          
+          />
+
+         
+
+          
+        </View>
+      );
+    }
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      marginTop: 20
+    },
+    searchBar:{
+      flexDirection:'row',
+      height:40,
+      width:'auto',
+      borderWidth:0.5,
+      alignItems:'center',
+      backgroundColor:'grey',
+  
+    },
+    bar:{
+      borderWidth:2,
+      height:30,
+      width:300,
+      paddingLeft:10,
+    },
+    searchButton:{
+      borderWidth:1,
+      height:30,
+      width:50,
+      alignItems:'center',
+      justifyContent:'center',
+      backgroundColor:'green'
+    }
+  })
